8e9db23509f0ed6f68c5f40c836db26e12a62553
illhammm <146872226+illhammm@users.noreply.github.com>
2023-10-04 12:21:28 +0300

    my change_

---
+++ nanozymes.py
@@ -0,0 +1,7 @@
+print('Abstract:'
+       'Artificial enzymes are nanoscale compounds that mimic the catalytic activity of enzymes'
+       ' and are considered to replace them in various fields. They not only have high catalytic activity' 
+       'and specificity, but also are convenient for mass production, storage and transportation. This project'
+       ' aims at materials design, for the rapid and directed development of artificial enzymes.'
+       'Nanomaterial parameters, organic coating, integration and assay conditions are evaluated to determine'
+       'their activity.')
