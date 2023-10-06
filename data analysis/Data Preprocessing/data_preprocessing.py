ca80a21d40354e55444853d4498d8b89c88eb0b2
illhammm <146872226+illhammm@users.noreply.github.com>
2023-10-06 11:04:14 +0300

    Пыталась вывести на консоль данные в'data_preprocessing' через pandas в комьюнити версии пайчарм, только со значениями словаря. Предыдущий выполненный принт закомментировала

--- data analysis/Data Preprocessing/data_preprocessing.py
+++ data analysis/Data Preprocessing/data_preprocessing.py
@@ -1,3 +1,16 @@
-import pandas as pd
-x = pd.read_csv("/Users/Home/Desktop/df_nnz.csv")
-print(x)
+# import csv
+#
+# import pandas as pd
+# x = pd.read_csv("/Users/Home/Desktop/df_nnz.csv")
+# print(x)
+# for line in x:
+#     #вывод в одну строку названий шапок массива данных для рассмотрения
+#     print(line[0::], end=' ')
+
+import csv
+with open("df_nnz.csv", "r") as f:
+    reader = csv.DictReader(f)
+    for line in reader:
+        #тут вывод с ключом значения и словарем
+        print(line, end='\n')
+
