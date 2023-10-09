86a90ba78e0c7da07209d75ec9c9f5b07328ccbd
illhammm <iamxilxamiya@mail.ru>
2023-10-09 14:43:08 +0300

    Провела анализ оставшихся пустых значений, всего 4 строки. Удалила эти строки.

--- Data_Preprocessing/data_preprocessing.py
+++ Data_Preprocessing/data_preprocessing.py
@@ -7,9 +7,15 @@ def to_numeric_column(df, column):
 df = to_numeric_column(df, 'Vmax')
 df = to_numeric_column(df, 'Km')
 
-#print(df.dtypes)
 
 
 df = df.dropna(subset=['Vmax', 'Km'], axis =0)
+df_nan = df.isna().mean().sort_values(ascending = False)
+#print(df_nan.head(10))
+
+df_colums = ['ph','Complexity','temp']
+nan_rows = df[df[df_colums].isnull().any(axis=1)]
+print(nan_rows[df_colums])
+df = df.dropna(axis =0)
 df_nan = df.isna().mean().sort_values(ascending = False)
 print(df_nan.head(10))
