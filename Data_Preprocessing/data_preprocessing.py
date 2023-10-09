7c06d878b55c8c79cdf7a5827c3e4d3e2c5f408c
KinsalMaties <srazetdinov@list.ru>
2023-10-09 15:48:33 +0300

    Возникли проблемы с присоединением к основной ветке, теперь работает как надо

--- Data_Preprocessing/data_preprocessing.py
+++ Data_Preprocessing/data_preprocessing.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 df = pd.read_csv("../Data_Analysis/df_nnz.csv")
 def to_numeric_column(df, column):
@@ -21,4 +24,8 @@ df_nan = df.isna().mean().sort_values(ascending = False)
 # print(df_nan.head(10))
 
 
-print(df[df.duplicated ()])
+#print(df[df.duplicated ()])
+
+df.loc[:, 'Vmax_log'] = np.log10(df['Vmax'])
+sns.boxplot(x=df['Vmax_log'])
+plt.show()
